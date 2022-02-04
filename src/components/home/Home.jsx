0314d9ddaefc01fdb5b398c12c8cfff637e49def
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-02-04 18:05:49 +0530

    add gitignore

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Home = () => {
+    return <div></div>;
+};
+exports.default = Home;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/home/Home";
+
+ReactDOM.render(React.createElement(Home), document.querySelector('.root'));
