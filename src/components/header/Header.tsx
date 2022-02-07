ec9172f76215c234bd5a61b52e9edcdd5256772a
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-02-07 14:40:24 +0530

    add Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Header.css';
+
+const Header = (): JSX.Element => {
+    return (
+        <header className="heamder">
+            <h2 className="trivia-chad-title">Trivia Chad</h2>
+        </header>
+    );
+};
+
+export default Header;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Phone from "../../containers/phone/Phone";
 import LesGoButton from "../buttons/les-go-button/LesGoButton";
+import Header from "../header/Header";
 
 import "./Home.css";
 
 const Home = (): JSX.Element => {
     return (
         <div className="home">
+            <Header></Header>
             <LesGoButton></LesGoButton>
             <Phone></Phone>
         </div>
