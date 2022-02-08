235dcc1d91ee2bd54428fc2388668b9505dfd8a3
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-02-08 07:39:20 +0530

    add click handler to options

--- src/containers/option/QOption.tsx
+++ src/containers/option/QOption.tsx
@@ -4,7 +4,7 @@ import "./QOption.css";
 
 const Option = (props: any): JSX.Element => {
     return (
-        <div className="option">
+        <div onClick={props.onClick} className="option">
             <p className="option-text">{props.children}</p>
             <div className={`is-chosen-circle ${props.isChosen ? 'on': ''}`}></div>
         </div>
--- src/containers/question-card/QuestionCard.tsx
+++ src/containers/question-card/QuestionCard.tsx
@@ -11,12 +11,29 @@ const QuestionCard = (props: any): JSX.Element => {
     let ctx = useContext(QuestionContext);
 
     let [question, setQuestion] = useState(ctx.questions[0]);
+    let [chosenData, setChosenData] = useState([false, false, false, false]);
 
     let parser = new DOMParser();
 
     useEffect(() => {
         setQuestion(ctx.questions[ctx.qNumber]);
     }, [ctx.qNumber, ctx.questions]);
+
+    const handleClick = (index: number): void => {
+        setChosenData((oldState: boolean[]): boolean[] => {
+
+            let enable = !oldState[index];
+
+            // oldState.fill(false) won't work as it'll still be
+            // the same reference, and essentially the same state
+            // to React
+
+            oldState = [false, false, false, false];
+            oldState[index] = enable;
+
+            return oldState;
+        });
+    };
 
     return (
         <Card width="80%" height="70%">
@@ -27,10 +44,30 @@ const QuestionCard = (props: any): JSX.Element => {
                             .textContent
                     }
                 </QuestionTitle>
-                <QOption isChosen={false}>{question.options[0]}</QOption>
-                <QOption isChosen={false}>{question.options[1]}</QOption>
-                <QOption isChosen={false}>{question.options[2]}</QOption>
-                <QOption isChosen={false}>{question.options[3]}</QOption>
+                <QOption
+                    onClick={() => handleClick(0)}
+                    isChosen={chosenData[0]}
+                >
+                    {question.options[0]}
+                </QOption>
+                <QOption
+                    onClick={() => handleClick(1)}
+                    isChosen={chosenData[1]}
+                >
+                    {question.options[1]}
+                </QOption>
+                <QOption
+                    onClick={() => handleClick(2)}
+                    isChosen={chosenData[2]}
+                >
+                    {question.options[2]}
+                </QOption>
+                <QOption
+                    onClick={() => handleClick(3)}
+                    isChosen={chosenData[3]}
+                >
+                    {question.options[3]}
+                </QOption>
             </div>
         </Card>
     );
