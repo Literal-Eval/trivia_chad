d1be0a7206968771aaf2a55a1bc2345481a184f4
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-02-08 12:54:21 +0530

    add gh link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = (): JSX.Element => {
     return (
         <header className="heamder">
             <h2 className="trivia-chad-title">Trivia Chad</h2>
+            <a className="gh-link" href="https://github.com/Literal-Eval/trivia_chad">
+                <i className="fab fa-github"></i>
+            </a>
         </header>
     );
 };
