01871de36f3139b54bbe700c5006628f7b6806ff
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-02-08 08:08:27 +0530

    fix option color while hover

--- src/containers/option/QOption.tsx
+++ src/containers/option/QOption.tsx
@@ -4,9 +4,9 @@ import "./QOption.css";
 
 const Option = (props: any): JSX.Element => {
     return (
-        <div onClick={props.onClick} className="option">
+        <div onClick={props.onClick} className={`option ${props.isChosen ? 'on': ''}`}>
             <p className="option-text">{props.children}</p>
-            <div className={`is-chosen-circle ${props.isChosen ? 'on': ''}`}></div>
+            <div className="is-chosen-circle"></div>
         </div>
     );
 };
